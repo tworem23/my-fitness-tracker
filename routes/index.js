b099cc17afc2cd87fd9373aef6754666fb7f2b69
tworem23 <josh.remington1997@gmail.com>
2023-12-05 16:23:10 -0800

    Oauth config

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
+const passport = require('passport');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    prompt: 'select_account'
+  }
+))
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/workouts',
+    failureRedirect: '/workouts'
+  }
+))
+
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/movies');
+  });
 });
 
 module.exports = router;
